2a360d3fe7f2d70c1ca7986e46d813c38431d7ff
Boris Kubiak <kubiakboris@gmail.com>
2018-11-01 10:15:24 +0100

    Parse Linky data properly

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const dayjs = require('dayjs');
 
 const get = (p, o) => p.reduce((xs, x) => (xs && xs[x]) ? xs[x] : null, o);
+const parseDate = dt => dayjs(`${dt.substr(6, 4)}-${dt.substr(3, 2)}-${dt.substr(0, 2)}`);
 
 async function login(email, password) {
 	const body = new URLSearchParams({
@@ -42,34 +43,38 @@ async function login(email, password) {
 class LinkySession {
 	constructor(cookies) {
 		this.cookies = cookies;
+		this.calledOnce = false;
 	}
 
-	async getHourlyData() {
-		const data = await this._getData('urlCdcHeure', dayjs().add(-1, 'day'), dayjs());
-		return LinkySession.parseData(data, 0.5, 'hour', '{YYYY} MM-DDTHH:mm:ss SSS [Z] A');
+	async getHourlyData(opts) {
+		opts = LinkySession.parseOpts(opts, dayjs().add(-1, 'day'), dayjs());
+		const data = await this.getData('urlCdcHeure', opts);
+		return LinkySession.parseData(data, 0.5, 'hour');
 	}
 
-	async getDailyData() {
-		const data = await this._getData('urlCdcJour', dayjs().add(-1, 'day').add(-1, 'month'), dayjs().add(-1, 'day'));
-		return LinkySession.parseData(data, 1, 'days', '{YYYY} MM-DDTHH:mm:ss SSS [Z] A');
+	async getDailyData(opts) {
+		opts = LinkySession.parseOpts(opts, dayjs().add(-1, 'day').add(-1, 'month'), dayjs().add(-1, 'day'));
+		const data = await this.getData('urlCdcJour', opts);
+		return LinkySession.parseData(data, 1, 'days');
 	}
 
-	async getMonthlyData() {
-		const data = await this._getData('urlCdcMois', dayjs().add(-11, 'month'), dayjs());
-		return LinkySession.parseData(data, 1, 'month', '{YYYY} MM-DDTHH:mm:ss SSS [Z] A');
+	async getMonthlyData(opts) {
+		opts = LinkySession.parseOpts(opts, dayjs().add(-11, 'month'), dayjs());
+		const data = await this.getData('urlCdcMois', opts);
+		return LinkySession.parseData(data, 1, 'month');
 	}
 
-	async getYearlyData() {
-		const data = await this._getData('urlCdcAn', null, null);
-		return LinkySession.parseData(data, 1, 'year', '{YYYY} MM-DDTHH:mm:ss SSS [Z] A');
+	async getYearlyData(_) {
+		const data = await this.getData('urlCdcAn', {});
+		return LinkySession.parseData(data, 1, 'year');
 	}
 
-	async _getData(resource, start, end) {
+	async getData(resource, opts) {
 		const reqPart = 'lincspartdisplaycdc_WAR_lincspartcdcportlet';
 
 		const body = new URLSearchParams();
-		body.append('_' + reqPart + '_dateDebut', start ? start.format('DD/MM/YYYY') : null);
-		body.append('_' + reqPart + '_dateFin', end ? end.format('DD/MM/YYYY') : null);
+		body.append('_' + reqPart + '_dateDebut', opts.start ? opts.start.format('DD/MM/YYYY') : null);
+		body.append('_' + reqPart + '_dateFin', opts.end ? opts.end.format('DD/MM/YYYY') : null);
 
 		const query = new URLSearchParams({
 			p_p_id: reqPart,
@@ -94,11 +99,12 @@ class LinkySession {
 				withCredentials: true
 			});
 		} catch (err) {
-			if (!err.response || err.response.status !== 302) {
+			if (!err.response || err.response.status !== 302 || this.calledOnce) {
 				throw new Error('Unexpected export response (2): ' + err.message);
 			}
 			const newCookies = err.response.headers['set-cookie'];
 			this.cookies = this.cookies.concat(newCookies);
+			this.calledOnce = true;
 
 			try {
 				// Call a second time with the new cookies
@@ -115,7 +121,11 @@ class LinkySession {
 			}
 		}
 
-		if (resp.data.indexOf('Conditions d\'utilisation') > -1) {
+		if (this.calledOnce) {
+			return resp.data;
+		}
+
+		if (JSON.stringify(resp.data).indexOf('Conditions') > -1) {
 			throw new Error('Please log in manually and accept the new terms of service');
 		}
 
@@ -126,18 +136,41 @@ class LinkySession {
 		return this.cookies.map(c => c.substr(0, c.indexOf(';'))).reduce((a, b) => a + '; ' + b);
 	}
 
-	static parseData(input, step, unit, format) {
-		console.log(input);
-		const start = dayjs(get(['graphe', 'periode', 'dateDebut'], input));
-		const offset = get(['graphe', 'decalage'], input) || 0;
-		console.log('start', start.format(format));
-		console.log('offset', offset);
+	static parseOpts(opts, start, end) {
+		opts = opts || {};
+		return {
+			start: opts.start ? parseDate(opts.start) : start,
+			end: opts.end ? parseDate(opts.end) : end
+		};
+	}
+
+	static parseData(input, step, unit) {
+		if (get(['etat', 'valeur'], input) === 'nonActive') {
+			throw new Error('No available data for the selected period');
+		}
+
 		const values = get(['graphe', 'data'], input);
 		if (!Array.isArray(values)) {
-			throw new TypeError(`Unexpected data: ${input}`);
+			throw new Error(`Unexpected data: ${JSON.stringify(input)}`);
 		}
-		// TODO
-		return values;
+
+		const start = parseDate(get(['graphe', 'periode', 'dateDebut'], input));
+		const offset = get(['graphe', 'decalage'], input) || 0;
+
+		values.splice(0, offset);
+		values.splice(-offset, offset);
+
+		const res = [];
+		let date = start;
+		for (const val of values) {
+			res.push({
+				date: date.format('YYYY-MM-DD HH:mm:ss'),
+				value: val.valeur < 0 ? null : val.valeur
+			});
+			date = date.add(step, unit);
+		}
+
+		return res;
 	}
 }
 
