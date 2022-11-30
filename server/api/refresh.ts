6092e75d96e79a577bf6a3a1d5b75c4078a051f5
Boris Kubiak <kubiakboris@gmail.com>
2022-11-30 17:18:52 +0100

    Add rate limit message

--- server/api/refresh.ts
+++ server/api/refresh.ts
@@ -4,6 +4,15 @@ import qs from 'qs';
 
 const { REDIRECT_URI, CLIENT_ID, CLIENT_SECRET, SANDBOX } = process.env;
 
+const REQUEST_LIMIT = 10;
+
+const requestCount: { [token: string]: number } = {};
+
+function isLimitReached(token: string): boolean {
+    requestCount[token] = requestCount[token] ? requestCount[token] + 1 : 1;
+    return requestCount[token] >= REQUEST_LIMIT;
+}
+
 export default (req: NowRequest, res: NowResponse) => {
     const { token } = req.query;
 
@@ -29,6 +38,21 @@ export default (req: NowRequest, res: NowResponse) => {
         .catch((e) => {
             const code = e.response ? e.response.status : 0;
             const body = e.response ? e.response.data : e.message;
+            if (code === 401) {
+                if (isLimitReached(token as string)) {
+                    res.status(429).send({
+                        response: `
+                            === Limite d'appels API atteinte ===
+                            Cela fait de nombreuses fois que vous tentez de rafraichir vos tokens alors que vos identifiants ont expiré.
+                            Merci de cesser immédiatement afin d'éviter un ban définitif du service
+                            Cordialement - @bokub`
+                            .trim()
+                            .split('\n')
+                            .map((x) => x.trim()),
+                    });
+                    return;
+                }
+            }
             console.error(`Erreur: impossible d'obtenir le token.\nCode = ${code}.\nError = ${body}`);
             res.status(code || 500).send(body);
         });
