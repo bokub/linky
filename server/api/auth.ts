fd0b577b5409369f41f9a56b7d8c715f8e8cfbcb
Boris Kubiak <kubiakboris@gmail.com>
2023-05-12 09:10:28 +0200

    Updates for V1

--- server/api/auth.ts
+++ server/api/auth.ts
@@ -3,7 +3,7 @@ import { NowRequest, NowResponse } from '@vercel/node';
 const { CLIENT_ID, SANDBOX } = process.env;
 
 export default (req: NowRequest, res: NowResponse) => {
-    const state = req.query.state || Array.from({ length: 8 }, () => Math.random().toString(36)[2]).join('');
+    const state = req.query.state || 'v1_' + Array.from({ length: 8 }, () => Math.random().toString(36)[2]).join('');
     const baseURI = SANDBOX ? 'https://gw.hml.api.enedis.fr' : 'https://mon-compte-particulier.enedis.fr';
     return res.redirect(
         `${baseURI}/dataconnect/v1/oauth2/authorize` +
